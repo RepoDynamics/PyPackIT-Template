8a8638a5de69ae00088bf13d8e9dc32aced1870e
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-09-19 23:26:39 +0200

    Update __init__.py

--- pkg/src/my_new_project/__init__.py
+++ pkg/src/my_new_project/__init__.py
@@ -1,2 +1,26 @@
+"""Main docstring."""
+
 __version_details__ = {"version": "0.0.0"}
+"""Version details."""
+
 __version__ = __version_details__["version"]
+
+
+def f(x: int) -> int:
+    """Function f.
+
+    Parameters
+    ----------
+    x : int
+        The input value.
+
+    Returns
+    -------
+    int
+        The output value.
+
+    References
+    ----------
+    - https://www.example.com
+    """
+    return x + 1
