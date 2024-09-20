93cfea0c4a3cfaf4fae05ed5dcc50d0bd07f765e
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-09-20 22:41:12 +0200

    Create __init__.py

---
+++ pkg/src/my_new_project/data/__init__.py
@@ -0,0 +1,27 @@
+"""Retrieve package data files."""
+
+from __future__ import annotations as _annotations
+
+from typing import TYPE_CHECKING as _TYPE_CHECKING
+
+import pkgdata as _pkgdata
+
+if _TYPE_CHECKING:
+    from pathlib import Path
+
+__all__ = ["get"]
+
+
+def get(relative_path: str) -> Path:
+    """Get the absolute path to a package data file.
+
+    Parameters
+    ----------
+    relative_path
+        Path to the file relative to the package's data directory.
+    """
+    path_data_dir = _pkgdata.get_package_path_from_caller(top_level=False)
+    filepath = path_data_dir / relative_path
+    if not filepath.is_file():
+        raise FileNotFoundError(f"Could not find package data file at {filepath}")
+    return filepath
