60cdf88110db36d12348ccd42a38c92ca231ab90
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-09-20 23:49:41 +0200

    Update __init__.py

--- pkg/src/my_new_project/__init__.py
+++ pkg/src/my_new_project/__init__.py
@@ -29,7 +29,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from my_new_project import data
 
-
 __version_details__: dict[str, str | tuple] = {"version": "0.0.0"}
 """Details of the currently installed version,
 including version number, date, branch, and commit hash."""
