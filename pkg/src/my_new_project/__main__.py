e25364664431be0fad7826f594f4a59d650d8e2e
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-09-21 01:34:24 +0200

    Create __main__.py

---
+++ pkg/src/my_new_project/__main__.py
@@ -0,0 +1,32 @@
+"""Main command-line interface of the package."""
+
+import argparse
+import sys
+
+from my_new_project import __version__
+
+
+def main():
+    """Main command-line interface of the package."""
+
+    # AUTOCODE START: cli
+    cli_description: str = "Command-line interface for the package."
+    cli_greeting: str = "Welcome to the package's command-line interface!"
+    cli_logo: str = ""
+    # AUTOCODE END: cli
+
+    parser = argparse.ArgumentParser(description=cli_description)
+    parser.add_argument(
+        '--version',
+        action='version',
+        version=f'%(prog)s {__version__}',
+        help="Show the current version of the package."
+    )
+    args = parser.parse_args()
+    if not vars(args):
+        print(cli_greeting)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
